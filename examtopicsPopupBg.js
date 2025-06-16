0c5fc32156294f59767918bc3267f5996d8e7b89
Arnav-Sangle <arnavsangle7x24@gmail.com>
2025-06-16 11:49:57 +0530

    Create examtopicsPopupBg.js

---
+++ examtopicsPopupBg.js
@@ -0,0 +1,3 @@
+//Make background transperent 
+document.getElementById('notRemoverPopup').style.backgroundColor = "rgba(0, 0, 0, 0)"
+// document.getElementById('notRemoverPopup').style.background = "none"
